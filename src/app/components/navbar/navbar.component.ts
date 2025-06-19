691c22e724466cc2939a70a024bbd500170146cd
Jeremy Lanssiers <jlanssie@gmail.com>
2025-06-19 22:22:25 +0200

    Added custom JS to an Angular component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+interface NavLink {
+  path: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  navLinks: NavLink[] = [
+    { path: '/', label: 'Home' },
+    { path: '/test', label: 'Test' },
+    { path: '/about', label: 'About Us' },
+    { path: '/contact', label: 'Contact Us' }
+  ];
 
+  constructor() { }
 }
